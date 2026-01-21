7170222150e64ded2e0a4a5779dba1f62348bbeb
andy <andy7d0@gmail.com>
2026-01-21 10:55:44 +0300

    more cchanges

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,27 +1,56 @@
 // @ts-check
-import { dirname } from 'node:path';
+import path from 'node:path';
+import { execaSync } from 'execa';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from '@rspack/cli';
 import { rspack } from '@rspack/core';
 import { ReactRefreshRspackPlugin } from '@rspack/plugin-react-refresh';
 // import oxlint  from 'unplugin-oxlint/webpack';
-import { OxLintWebpackPlugin } from "oxlint-rspack-plugin";
+//import { OxLintWebpackPlugin } from "oxlint-rspack-plugin";
+import { OxLintWebpackPlugin } from "./dev/compile/oxlint-rspack-plugin/index.js";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isDev = process.env.NODE_ENV === 'development';
 
 // Target browsers, see: https://github.com/browserslist/browserslist
-const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR'];
+const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR']; //, 'baseline widely available'];
+
+let gitVersions = 
+{
+  shortHash: '#DEV'
+  , numCommits: '~inf~'
+  , dirty: true
+}
+if(!isDev){
+  gitVersions = {
+    shortHash: execaSync('git', ['rev-parse', '--short', 'HEAD']).stdout
+    , numCommits: Number(execaSync('git', ['rev-list', 'HEAD', '--count']).stdout)
+    , dirty: execaSync('git', ['status', '-s', '-uall']).stdout.length > 0
+  }
+};
+
 
 export default defineConfig({
   context: __dirname,
   devServer: {
-    host: '127.1.2.1',
     port: 3000,
     open: false, // open: ['/my-page', '/another-page']
+    setupMiddlewares: (middlewares) => {
+      middlewares.unshift({
+        name: 'local-redir',
+        // `path` is optional
+        path: '/',
+        middleware: (req, res, next) => {
+          if(req.path === '/') return next();
+          if(req.path.match(/[.]/)) return next();
+          res.redirect(302, `/?${req.path}`);
+        },
+      });
+      return middlewares;
+    }
   },
   entry: {
-    main: './src/main.jsx',
+    main: './src/client/main.jsx',
   },
   resolve: {
     extensions: ['...', '.ts', '.tsx', '.jsx'],
@@ -34,12 +63,19 @@ export default defineConfig({
       },
       {
         test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules[\\/]core-js/,
         use: [
           {
             loader: 'builtin:swc-loader',
             /** @type {import('@rspack/core').SwcLoaderOptions} */
             options: {
+              env: {
+                mode: 'entry',
+                coreJs: '3.47.0',
+                targets
+              },
               jsc: {
+                //externalHelpers: true, //BUG in bundler
                 parser: {
                   syntax: 'typescript',
                   tsx: true,
@@ -49,10 +85,15 @@ export default defineConfig({
                     runtime: 'automatic',
                     development: isDev,
                     refresh: isDev,
+                    useBuiltins: true,
                   },
                 },
+                experimental: {
+                  plugins: [
+                    [path.resolve(__dirname, "dev/compile/api-call/target/wasm32-wasip1/release/api_call.wasm"), {}],
+                  ]
+                }
               },
-              env: { targets },
             },
           },
         ],
@@ -69,7 +110,13 @@ export default defineConfig({
     new rspack.CircularDependencyRspackPlugin({
       failOnError: true,
       exclude: /node_modules/,
-    })
+    }),
+    new rspack.experiments.VirtualModulesPlugin({
+      'node_modules/git-version.js': 'module.exports = ' + JSON.stringify(gitVersions),
+    }),
+    new rspack.ProvidePlugin({
+      API: [path.resolve(path.join(__dirname, 'src/azlib/api.mjs')), 'API']
+    }),
   ],
   optimization: {
     minimizer: [
