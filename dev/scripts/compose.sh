dd17e4c40e8f1c2875c28d4972da3fbf341e4d0b
andy <andy7d0@gmail.com>
2026-02-20 12:47:38 +0300

    remove setup.sh

--- dev/scripts/compose.sh
+++ dev/scripts/compose.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
-S_UID="$(id -u)" \
-S_GID="$(id -g)" \
-S_ZONE="${PWD##*/}" \
+export S_UID="$(id -u)"
+export S_GID="$(id -g)"
+export S_ZONE="${PWD##*/}"
+
+mkdir -p workarea/sockets/web
+mkdir -p workarea/sockets/app
+mkdir -p workarea/sockets/pg
+
+rm -rf workarea/sockets/web/*
+rm -rf workarea/sockets/app/*
+rm -rf workarea/sockets/pg/*
+
+mkdir -p workarea/migration/dumps
+
+mkdir -p workarea/cache/ext
+mkdir -p workarea/cache/int
+mkdir -p workarea/cache/par
+
+mkdir -p workarea/modules/ext
+mkdir -p workarea/modules/int
+mkdir -p workarea/modules/par
+
+mkdir -p dist/web
+mkdir -p dist/app/ext
+mkdir -p dist/app/int
+mkdir -p dist/app/par
+mkdir -p dist/databases/main_db
+
+
 docker compose -f docker-compose-top.yml --env-file compose-env "$@"
